e1cc596ea810c22a6fd06423d9f3e34d0615592e
Igor C2 <igorc2@gmail.com>
2018-06-04 03:05:52 -0300

    Initial commit

---
+++ Budapest/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Budapest.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budapest.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (city == null)
+                return NotFound();
+            return Ok(city);
+        }
+    }
+}
---
+++ Budapest/Models/CitiesDataStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Budapest.API.Models
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name = "Virginópolis"},
+                new CityDto() { Id = 2, Name = "Governador Valadares"},
+            };
+        }
+    }
+}
